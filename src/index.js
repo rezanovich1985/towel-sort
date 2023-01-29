b7d10bf0801c3071f7a62b42ffc5a870ba1a6d3f
Резанович Игорь <Rezanovich@jukola.office>
2023-01-29 22:26:36 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let result = [];
+    for (let iRow = 0; iRow < matrix.length; iRow++) {
+        let row = matrix[iRow];
+        if (iRow % 2 === 1) {
+            row.reverse();
+        }
+        result = result.concat(row);
+    }
+
+    return result;
+};
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(module.exports(matrix));
